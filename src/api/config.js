57c54669fc794ba7889ec091cd15788c58eae3d9
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-05-31 22:43:03 +0200

    API auth added

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,8 @@
 export const APISettings = {
     token: null,
     headers: new Headers({
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'Authorization': ''
     }),
     baseURL: import.meta.env.VITE_API_HOST,
 }
